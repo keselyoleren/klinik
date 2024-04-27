c89e1e4b1187631141e1eca859a8f623f4abc658
M Hadi Sasmito <keselyoleren@gmail.com>
2024-04-27 20:26:18 +0700

    assesment fisioterapi

--- src/pasien/views/assesment_fisioterapi_views.py
+++ src/pasien/views/assesment_fisioterapi_views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import login, authenticate
 from django.forms import inlineformset_factory
 from django.shortcuts import get_object_or_404
 
+from django.utils.timezone import localtime
+from datetime import datetime
+from config.documents import GoogleDocumentProvider
 
 from django.contrib import messages
 from config.permis import IsAuthenticated, IsAuthenticated
@@ -108,21 +111,75 @@ class AssesMentFisioTerapiDeleteView(IsAuthenticated, DeleteView):
         return context
 
 
-class DownloadAssesmentAwalVisioterapiView(IsAuthenticated, GeneratePDF,  UpdateView):
+class DownloadAssesmentAwalVisioterapiView(IsAuthenticated,  UpdateView):
     model = AssesMentFisioTerapi
     template_name = 'fisio_terapi/export/assesment.html'
     context_object_name = 'assesment'
     form_class = AssesmentFisioterapiForm
 
     def get(self, request, *args, **kwargs):
-        return self.render_to_pdf(
-            {
-                'assesment': self.get_object(),
-                'pasien':self.get_object().pasien_fisioterapi,
-                'host' : f"{self.request.scheme}://{self.request.META['HTTP_HOST']}"
-            },
-            self.template_name,
-            '/css/pdf.css',
-            f'Assesment Pasien Fisioterapi {self.get_object().pasien_fisioterapi.pasien.full_name}'
-        )
+        document_id = '1ZyaFFt8ZSREChhXpjTiWzxtevDhO7qMisWINLOhB6Uo'
+        created_at_local = localtime(self.get_object().created_at)
+        file_name = f'ASSESMENT AWAL FISIOTERAPI - {self.get_object().pasien_fisioterapi.pasien} ({datetime.now()})'
+        params = {
+            'created_at':created_at_local,
+            'nama':self.get_object().pasien_fisioterapi.pasien.full_name,
+            'alamat':self.get_object().pasien_fisioterapi.pasien.alamat,                
+            'jenis_kelamin':self.get_object().pasien_fisioterapi.pasien.jenis_kelamin, 
+
+            'keluhan_utama':self.get_object().keluhan_utama,                
+            'riwayat_penyakit_sekarang':self.get_object().riwayat_penyakit_sekarang,                
+            'riwayat_penyakit_dahulu':self.get_object().riwayat_penyakit_dahulu,                
+
+            'td':self.get_object().td,                
+            'hr':self.get_object().hr,                
+            'suhu':self.get_object().suhu,                
+            'rr':self.get_object().rr,                
+            'pr':self.get_object().pr,                
+            'sekor_nyeri':self.get_object().sekor_nyeri,      
+
+            # kemampuan fungsional
+            'alat_bantu':self.get_object().alat_bantu,      
+            'prothese':self.get_object().prothese,
+            'deformitas':self.get_object().deformitas,      
+            'resiko_jatuh':self.get_object().resiko_jatuh,
+            'lain_lain':self.get_object().lain_lain,
+
+            # pemeriksaan sistematik khusus
+            'musco':self.get_object().muscoloskeletal,
+            'neuro':self.get_object().neoromuscular,
+            'cardi':self.get_object().cardiopulmonal,
+            'inte':self.get_object().integument,
+
+            'mes_musco':self.get_object().mes_muscoloskeletal,
+            'mes_neuro':self.get_object().mes_neoromuscular,
+            'mes_cardi':self.get_object().mes_cardiopulmonal,
+            'mes_inte':self.get_object().mes_integument,
+
+            # data penunjang
+            'radiologi':self.get_object().radiologi,
+            'emg':self.get_object().emg,
+            'lab':self.get_object().laboratorium,
+            'lain_lain':self.get_object().lain_lain,
+
+            'diag_fisio':self.get_object().diagnosis_fisioterapi,
+            'prog_ren_terapi':self.get_object().program_rencana_terapi,
+            'evaluasi':self.get_object().evaluasi,
+
+
+                  
+        }
+        document = GoogleDocumentProvider(document_id, params, file_name)
+        proses_document = document.process_document()
+        return document.download_google_docs_as_pdf(proses_document)
+        # return self.render_to_pdf(
+        #     {
+        #         'assesment': self.get_object(),
+        #         'pasien':self.get_object().pasien_fisioterapi,
+        #         'host' : f"{self.request.scheme}://{self.request.META['HTTP_HOST']}"
+        #     },
+        #     self.template_name,
+        #     '/css/pdf.css',
+        #     f'Assesment Pasien Fisioterapi {self.get_object().pasien_fisioterapi.pasien.full_name}'
+        # )
 
