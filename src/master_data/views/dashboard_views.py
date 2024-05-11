fbcf512aed25d1eef16d53734c076b122c2d7575
M Hadi Sasmito <keselyoleren@gmail.com>
2024-05-11 13:42:08 +0700

    edit dashboard

--- src/master_data/views/dashboard_views.py
+++ src/master_data/views/dashboard_views.py
@@ -3,6 +3,7 @@
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from config.permis import IsAuthenticated, IsAuthenticated
+from config.choice import DAY_CODE
 from master_data.models import *
 from pasien.models import *
 
@@ -16,7 +17,19 @@ class InventoryObatListView(IsAuthenticated, TemplateView):
         context['pasien_rawat_jalan'] = RawatInap.objects.count()
         context['pasien_rawat_inap'] = RawatJalan.objects.count()
         context['pasien_fisioterapi'] = PasienFisioterapi.objects.count()
+        context['tenaga_medis'] = TenagaMedis.objects.count()
 
-
+        # jadwal
+        context['header'] = 'Jadwal Tenaga Medis'
+        context['header_title'] = 'Jadwal Tenaga Medis'
+        context['jadwal'] = [
+            {
+                'title': f"{jadwal.tenaga_medis}",
+                'hari': jadwal.hari,
+                'code_hari': DAY_CODE.get(jadwal.hari, None),
+                'duration': f"{jadwal.jam_mulai.strftime('%H:%M')} - {jadwal.jam_selesai.strftime('%H:%M')}",
+                
+            } for jadwal in JadwalTenagaMedis.objects.filter(is_active=True)
+        ]
         return context
     
